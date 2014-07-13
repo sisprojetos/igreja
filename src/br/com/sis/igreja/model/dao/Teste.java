69d1906220767e8207f759bed99b5e658b5aeb39
Andrew <andrewsato10@gmail.com>
2014-07-13 13:59:01 -0300

    Alterado o comentário da classe Teste para testar o update

--- src/br/com/sis/igreja/model/dao/Teste.java
+++ src/br/com/sis/igreja/model/dao/Teste.java
@@ -3,7 +3,7 @@ package br.com.sis.igreja.model.dao;
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubs
 
 	}
 
