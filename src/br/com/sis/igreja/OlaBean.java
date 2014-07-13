1556b4527287b94958308b24734a17ad429154e9
Andrew <andrewsato10@gmail.com>
2014-07-13 12:38:55 -0300

    Adicionado o atributo telefone no index e no olabean

--- src/br/com/sis/igreja/OlaBean.java
+++ src/br/com/sis/igreja/OlaBean.java
@@ -12,6 +12,15 @@ public class OlaBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String nome;
+	private String telefone;
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 
 	public String getNome() {
 		return nome;
